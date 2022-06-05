5daa436bc53fa66725c7d4d71fd7a77c708f7ba8
nickdas1 <75402464+nickdas1@users.noreply.github.com>
2022-06-05 14:26:49 -0400

    set up stream create form

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 
 import App from "./components/App";
 import reducers from "./reducers";
 
-const store = configureStore({reducer: reducers});
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = configureStore(
+    { reducer: reducers },
+    composeEnhancers(applyMiddleware())
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
