deec064ef6986338627265be7c65a0c53be3fe5e
nickdas1 <75402464+nickdas1@users.noreply.github.com>
2022-06-06 19:07:00 -0400

    set up show stream

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchStream } from "../../actions";
 
 export default function StreamShow() {
-    return (
-        <div>StreamShow</div>
-    );
-};
+    const dispatch = useDispatch();
+    const { id } = useParams();
+    const stream = useSelector((state) => state.streams[id]);
+
+    useEffect(() => {
+        dispatch(fetchStream(id));
+    }, [id, dispatch]);
+
+    const renderContent = () => {
+        if (!stream) {
+            return <div>Loading...</div>;
+        }
+
+        const { title, description } = stream;
+
+        return (
+            <div>
+                <h1>{title}</h1>
+                <h5>{description}</h5>
+            </div>
+        );
+    };
+
+    return renderContent();
+}
